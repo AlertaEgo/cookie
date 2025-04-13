79d88a630989cb82d914c54961b3e4c88c657852
AlertaEgo <anntemkin04@gmail.com>
2025-04-13 20:06:41 +0300

    (feat:first-task):added reduce realization

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-export const counterOfIncreasing = (measurments: number[]) : number => {
+export const counterOfIncreasingMapRealization = (measurments: number[]) : number => {
     let count = 0;
     measurments.map((num, index) => {
         if(num > measurments[index - 1]){
@@ -8,4 +8,19 @@ export const counterOfIncreasing = (measurments: number[]) : number => {
     return count;
 }
 
-console.log(counterOfIncreasing([2,3,4,5,0,-1,8]));
+
+export const counterOfIncreasingReduceRealization = (measurments: number[]) : number => {
+    let count = 0;
+    count = measurments.reduce((previousValue, currentValue) => {
+        if(currentValue > previousValue){
+            count++;
+        }
+        return count;
+    },
+     measurments[0] +1
+    );
+    return count;
+}
+
+console.log(counterOfIncreasingReduceRealization([2,3,4,5,0,-1,8]));
+console.log(counterOfIncreasingMapRealization([2,3,4,5,0,-1,8]));
